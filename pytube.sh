bf80111e279bdafd2fb6f520426818c415d5bb73
Jeiel <github.esvlc@slmail.me>
2023-05-24 10:09:02 -0300

    Create pytube.sh

---
+++ pytube.sh
@@ -0,0 +1,21 @@
+echo 'Precisa do "Pytube" e do "git" instalado'
+echo 'apt install git clone git://github.com/pytube/pytube.git'
+sleep 5
+pytube https://youtu.be/PT8x7HKYVfA
+pytube https://youtu.be/jyISu8FYeI4
+pytube https://youtu.be/EfpKw0Da748
+pytube https://youtu.be/CVOQNLX5Hlg
+pytube https://youtu.be/zppb13bVOic
+pytube https://youtu.be/AHxHgGQWJTo
+pytube https://youtu.be/Fs_UXhMUXLM
+pytube https://youtu.be/UPi54j8fzBc
+pytube https://youtu.be/AFFDnORWTFE
+pytube https://youtu.be/4_lQ6CRcJNM
+pytube https://youtu.be/RWKqGK29U8c
+pytube https://youtu.be/IYrOwg2VpvE
+pytube https://youtu.be/RAgEl-nE9Gk
+clear
+find /home -name .mp4
+echo '=========================='
+echo 'Músicas baixada!'
+echo '=========================='
